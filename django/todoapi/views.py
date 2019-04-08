9f7dd9f6f60846ea58b7be251e95ed66999f8326
suvasishpradhan033@gmail.com <suvasishpradhan033@gmail.com>
2019-04-08 10:42:23 +0530

    intial commit

---
+++ django/todoapi/views.py
@@ -0,0 +1,30 @@
+from django.shortcuts import render
+
+# Create your views here.
+from .models import Task
+from .serializers import TaskSerializer
+
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+
+class TaskList(APIView):
+    """
+    View all tasks.
+    """
+    def get(self, request, format=None):
+        """
+        Return a list of all tasks.
+        """
+        tasks = Task.objects.all()
+        serializer = TaskSerializer(tasks, many=True)
+        return Response(serializer.data)
+    def post(self, request, format=None):
+        """
+        Create a task.
+        """
+        serializer = TaskSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
